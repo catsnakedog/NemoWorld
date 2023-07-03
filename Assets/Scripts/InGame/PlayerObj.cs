a789e1f2a306cc35e6f8cab0670c8a651174fe4d
catsnakedog <0812kdh@gmail.com>
2023-07-03 12:00:29 +0900

    버그 임시 수정

---
+++ Assets/Scripts/InGame/PlayerObj.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerObj : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D Obj)
+    {
+        
+    }
+
+    void PlayerHurt()
+    {
+
+    }
+
+    void PlayerGetItem()
+    {
+
+    }
+}
