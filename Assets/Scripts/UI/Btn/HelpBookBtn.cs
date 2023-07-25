946ac7800610d6f9b3952b46776cc4fd6c3b5269
catsnakedog <103244627+catsnakedog@users.noreply.github.com>
2023-07-25 23:46:09 +0900

    도움말 제작

---
+++ Assets/Scripts/UI/Btn/HelpBookBtn.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class HelpBookBtn : EventTriggerEX
+{
+    [SerializeField]
+    int number;
+
+    [SerializeField]
+    GameObject middle;
+    [SerializeField]
+    GameObject right;
+
+    void Start()
+    {
+        init();
+    }
+
+    protected override void OnPointerDown(PointerEventData data)
+    {
+        right.transform.GetChild(0).gameObject.SetActive(false);
+        right.transform.GetChild(1).gameObject.SetActive(false);
+        right.transform.GetChild(2).gameObject.SetActive(false);
+        right.transform.GetChild(3).gameObject.SetActive(false);
+        right.transform.GetChild(4).gameObject.SetActive(false);
+
+        right.transform.GetChild(number).gameObject.SetActive(true);
+
+        StringBuilder sb = new StringBuilder("Book");
+        sb.Append(number);
+
+        middle.transform.GetChild(0).GetComponent<Image>().sprite = MainController.main.resource.sprite[sb.ToString()];
+    }
+}
