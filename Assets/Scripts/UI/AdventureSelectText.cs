e29edeba0be89dd0d591f4520e671994a5aeb2b3
kys97 <81239623+kys97@users.noreply.github.com>
2023-08-04 02:51:44 +0900

    모험모드 보상 UI

--- Assets/Scripts/UI/AdventureSelectText.cs
+++ Assets/Scripts/UI/AdventureSelectText.cs
@@ -3,12 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 using System.Text;
+using UnityEngine.UI;
 
 public class AdventureSelectText : MonoBehaviour
 {
     protected void Start()
     {
         QuestInfoTextSet();
+        SetRewardImage();
     }
 
     void QuestInfoTextSet()
@@ -33,5 +35,23 @@ public class AdventureSelectText : MonoBehaviour
         DataManager.Single.Data.inGameData.crruentQuest.gameMode = DataManager.Single.Data.questData.questInfo[i].gameMode;
         DataManager.Single.Data.inGameData.crruentQuest.info = DataManager.Single.Data.questData.questInfo[i].info;
         DataManager.Single.Data.inGameData.crruentQuest.time = DataManager.Single.Data.questData.questInfo[i].time;
+    }
+
+    void SetRewardImage()
+    {
+        Image rewardImage = GameObject.Find("RewardTicket").GetComponent<Image>();
+
+        switch (DataManager.Single.Data.inGameData.stage)
+        {
+            case 1:
+                rewardImage.sprite = MainController.main.resource.sprite[Define.SpriteDict.HeadTicket.ToString()];
+                break;
+            case 2:
+                rewardImage.sprite = MainController.main.resource.sprite[Define.SpriteDict.ClothTicket.ToString()];
+                break;
+            case 3:
+                rewardImage.sprite = MainController.main.resource.sprite[Define.SpriteDict.WingTicket.ToString()];
+                break;
+        }
     }
 }
