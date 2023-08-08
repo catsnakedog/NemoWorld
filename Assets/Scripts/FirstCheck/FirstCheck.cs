bccd743f067e6722c76ed4273e99e9c6b5b3355b
catsnakedog <103244627+catsnakedog@users.noreply.github.com>
2023-08-08 18:25:11 +0900

    수정사항

--- Assets/Scripts/FirstCheck/FirstCheck.cs
+++ Assets/Scripts/FirstCheck/FirstCheck.cs
@@ -11,15 +11,6 @@ public class FirstCheck : MonoBehaviour
         {
             DataManager.Single.Data.optionData.volumeBGM = 0.5f;
             DataManager.Single.Data.optionData.volumeSFX = 0.5f;
-            DataManager.Single.Data.inGameData.adventureClearList.Add("stage1");
-            DataManager.Single.Data.inGameData.adventureClearList.Add("stage2");
-            DataManager.Single.Data.inGameData.adventureClearList.Add("stage3");
-            DataManager.Single.Data.inGameData.missionClearList.Add("stage1");
-            DataManager.Single.Data.inGameData.missionClearList.Add("stage2");
-            DataManager.Single.Data.inGameData.missionClearList.Add("stage3");
-            DataManager.Single.Data.inGameData.storyClearList.Add("stage1");
-            DataManager.Single.Data.inGameData.storyClearList.Add("stage2");
-            DataManager.Single.Data.inGameData.storyClearList.Add("stage3");
             MainController.main.UI.UIsetting(Define.UIlevel.Level1, Define.UItype.FirstUI);
         }
         else
